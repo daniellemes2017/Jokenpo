09c15152207ee9cef3dbf88778bdf4cd3b017969
daniellemes2017 <daniel_lemes2017@outlook.com>
2024-06-12 14:02:59 -0300

    criei um enums

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ const machineScore = document.querySelector('#machine-score')
 let humanScoreNumber = 0
 let machineScoreNumber = 0
 
+const GAME_OPTIONS = {  // CRIEI UM ENUMS
+    ROCK: 'rock',
+    PAPER: 'paper',
+    SCISSORS: 'scissors'
+}
+
 const playHuman = (humanChoice) => {
    
     
@@ -12,7 +18,7 @@ const playHuman = (humanChoice) => {
 }
 
 const playMachine = () => {
-    const choices = ['rock', 'paper', 'scissors']
+    const choices = [GAME_OPTIONS.ROCK, GAME_OPTIONS.PAPER, GAME_OPTIONS.SCISSORS]
     const randomNumber = Math.floor(Math.random() * 3) 
 
     return choices[randomNumber]
@@ -26,9 +32,9 @@ const playTheGame = (human, machine) => {
         result.innerHTML = 'Deu empate!'
 
     } else if ( 
-      (human === 'paper' && machine === 'rock') || 
-      (human === 'rock' && machine === 'scissors') ||
-      (human === 'scissors' && machine === 'paper')
+      (human === GAME_OPTIONS.PAPER && machine === GAME_OPTIONS.ROCK) || 
+      (human === GAME_OPTIONS.ROCK && machine === GAME_OPTIONS.SCISSORS) ||
+      (human === GAME_OPTIONS.SCISSORS && machine === GAME_OPTIONS.PAPER)
     ) {
         humanScoreNumber++
         humanScore.innerHTML = humanScoreNumber
